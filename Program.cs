35eafbcb01648f76100c0483b870a92d889a32de
w wu <wwuabos@hotmail.com>
2023-10-11 17:22:31 -0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("1.");
+Console.WriteLine(typeof(System.Data.CommandBehavior).Assembly.Location);
+Console.WriteLine("2.");
+Console.WriteLine(typeof(System.Collections.ArrayList).Assembly.Location);
+Console.WriteLine("3.");
+Console.WriteLine(typeof(HashSet<>).Assembly.Location);
+Console.WriteLine("4.");
+foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+{
+	Console.WriteLine(assembly.Location);
+}
+Console.ReadKey();
